688bce51e301cbebdc53a1a2df387126715d039d
Josiah Caballero <Josiahc.1224@gmail.com>
2026-03-28 19:21:31 -0400

    binmary heap , data gen, and testing

---
+++ cmake-build-debug/CMakeFiles/DSA_Project_2.dir/multi_ary.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/DSA_Project_2.dir/multi_ary.cpp.obj: \
+ C:\Users\josia\OneDrive\Documents\DSA_Project_2\multi_ary.cpp \
+ C:\Users\josia\OneDrive\Documents\DSA_Project_2\multi_ary.h
--- heap.h
+++ heap.h
@@ -1,12 +1,23 @@
-#ifndef HEAP_H
-#define HEAP_H
+#ifndef BINARYHEAP_H
+#define BINARYHEAP_H
 
+#include <vector>
+using namespace std;
 
+class BinaryHeap {
+public:
+    vector<int> heap;
+    int comparisons;
+    int swaps;
 
-class heap {
+    BinaryHeap();
 
+    void insert(int val);
+    int deleteMin();
+
+private:
+    void heapifyUp(int i);
+    void heapifyDown(int i);
 };
 
-
-
-#endif //HEAP_H
+#endif
